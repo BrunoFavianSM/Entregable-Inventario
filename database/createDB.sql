0431fd85ba2c5793ba1351dadba2b9443074a0d3
BrunoFavianSM <Bruno.saldarriaga20@gmail.com>
2025-10-10 09:30:24 -0500

    cambios realizados

---
+++ database/createDB.sql
@@ -0,0 +1,4 @@
+-- Crear base de datos
+
+CREATE DATABASE IF NOT EXISTS nova_salud_inventario CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE nova_salud_inventario;
--- database/schema.sql
+++ database/schema.sql
@@ -4,15 +4,6 @@
 -- ============================================
 
 
--- Crear base de datos
--- 
---CREATE DATABASE nova_salud_inventario CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
-
-
-CREATE DATABASE IF NOT EXISTS nova_salud_inventario CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE nova_salud_inventario;
-
 -- ============================================
 -- TABLA: Categorías de Productos
 -- ============================================
